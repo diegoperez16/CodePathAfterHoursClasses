15a1dd86e60d97e1d2f394723631937e7845e4a1
diegoperez16 <diego.perez16@upr.edu>
2025-12-02 15:30:58 -0400

    Remove Instructor login from all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { BossProvider } from './context/BossContext';
 import { AdminProvider } from './context/AdminContext';
 import BossCreationPage from './pages/BossCreationPage';
@@ -7,18 +7,29 @@ import ScoreboardPage from './pages/ScoreboardPage';
 import TournamentPage from './pages/TournamentPage';
 import AdminControls from './components/AdminControls';
 
+function AppContent() {
+  const location = useLocation();
+  const showAdminControls = location.pathname !== '/';
+
+  return (
+    <>
+      {showAdminControls && <AdminControls />}
+      <Routes>
+        <Route path="/" element={<BossCreationPage />} />
+        <Route path="/fight" element={<FightSimulatorPage />} />
+        <Route path="/scoreboard" element={<ScoreboardPage />} />
+        <Route path="/tournament" element={<TournamentPage />} />
+      </Routes>
+    </>
+  );
+}
+
 function App() {
   return (
     <AdminProvider>
       <BossProvider>
         <Router>
-          <AdminControls />
-          <Routes>
-            <Route path="/" element={<BossCreationPage />} />
-            <Route path="/fight" element={<FightSimulatorPage />} />
-            <Route path="/scoreboard" element={<ScoreboardPage />} />
-            <Route path="/tournament" element={<TournamentPage />} />
-          </Routes>
+          <AppContent />
         </Router>
       </BossProvider>
     </AdminProvider>
